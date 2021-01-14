468f969a28da0532473865f7c1604441e364e687
yalitzab <yalitzabueno@gmail.com>
2021-01-13 21:13:15 -0800

    install apollo

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,14 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+type Query {
+    thoughts: # what goes here?
+  }
+
+`;
+
+
+// export the typeDefs
+module.exports = typeDefs;
